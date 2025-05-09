2a7a8b61f222bfb1ad6c6e341d9f6cb6df3d4b28
derrick-ng <ng.derrick1@hotmail.com>
2025-05-09 14:19:45 -0700

    Fix incorrect month value from getDate function

--- lib/getDate.ts
+++ lib/getDate.ts
@@ -1,6 +1,6 @@
 export function getDate() {
   const newDate = new Date();
-  const day = newDate.getDate();
+  const day = newDate.getDate() + 1;
   const month = newDate.getMonth();
   const year = newDate.getFullYear();
 
