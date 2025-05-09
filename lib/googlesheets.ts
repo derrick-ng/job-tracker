361c6048943a62f4859b0620ec53e333a4624444
derrick-ng <ng.derrick1@hotmail.com>
2025-05-09 12:11:33 -0700

    Create function to update status column into a dropdown menu

--- lib/googlesheets.ts
+++ lib/googlesheets.ts
@@ -12,18 +12,17 @@ interface GoogleSheetsProp {
   status: string;
   url: string;
 }
+const auth = new google.auth.GoogleAuth({
+  credentials: {
+    client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+  },
+  scopes: ["https://www.googleapis.com/auth/drive", "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/spreadsheets"],
+});
 
 export async function insertIntoGoogleSheets({ jobInfo, date, status, url }: GoogleSheetsProp) {
   const { company, role, location } = jobInfo;
   const salary = jobInfo.salary || "not provided";
-
-  const auth = new google.auth.GoogleAuth({
-    credentials: {
-      client_email: process.env.GOOGLE_CLIENT_EMAIL,
-      private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-    },
-    scopes: ["https://www.googleapis.com/auth/drive", "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/spreadsheets"],
-  });
 
   const sheets = google.sheets({
     auth,
@@ -37,4 +36,46 @@ export async function insertIntoGoogleSheets({ jobInfo, date, status, url }: Goo
       values: [[date, company, role, location, salary, status, url]],
     },
   });
+
+  await updateStatusWithDropdown();
+}
+async function updateStatusWithDropdown() {
+  const sheets = google.sheets({
+    auth,
+    version: "v4",
+  });
+
+  const sheetId = 0;
+  const ruleRequest = {
+    spreadsheetId: process.env.GOOGLE_SHEET_ID,
+    resource: {
+      requests: [
+        {
+          setDataValidation: {
+            range: {
+              sheetId,
+              startRowIndex: 1,
+              endRowIndex: 1000,
+              startColumnIndex: 5,
+              endColumnIndex: 6,
+            },
+            rule: {
+              condition: {
+                type: "ONE_OF_LIST",
+                values: [
+                  { userEnteredValue: "pending" },
+                  { userEnteredValue: "oa" },
+                  { userEnteredValue: "interview" },
+                  { userEnteredValue: "accepted" },
+                  { userEnteredValue: "rejected" },
+                ],
+              },
+            },
+          },
+        },
+      ],
+    },
+  };
+
+  await sheets.spreadsheets.batchUpdate(ruleRequest);
 }
