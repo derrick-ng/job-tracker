d5f5c4b979dcceca142a89ce407bf3007c8d4bb0
derrick-ng <ng.derrick1@hotmail.com>
2025-07-10 21:19:51 -0700

    Add domain/url check before scraping info

--- public/script.js
+++ public/script.js
@@ -15,21 +15,30 @@ document.getElementById("scrape-button").addEventListener("click", async () => {
   console.log("inside extension console");
 
   const { tabId, tabUrl } = await getCurrentTabInfo();
+
+  const newUrl = new URL(tabUrl);
+  const domain = newUrl.hostname;
+
   const date = getDate();
+  const status = "pending";
 
   //function/script to be inserted into browser from extension
   // **(add later)** add tabUrl as a parameter, querySelector will be based off tabUrl
-  function modifyDOM() {
+  function modifyDOM(domain) {
     console.log("script start");
-    const salaryParentElement = document.querySelector(".job-details-fit-level-preferences");
-    const salary = salaryParentElement?.querySelector("button:first-of-type span.tvm__text--low-emphasis strong")?.innerText ?? "n/a";
 
-    const locationParentElement = document.querySelector(".job-details-jobs-unified-top-card__primary-description-container");
-    const location = locationParentElement?.querySelector("span.tvm__text--low-emphasis")?.innerText ?? "n/aaa";
+    if (domain == "www.linkedin.com") {
+      const salaryParentElement = document.querySelector(".job-details-fit-level-preferences");
+      const salary = salaryParentElement?.querySelector("button:first-of-type span.tvm__text--low-emphasis strong")?.innerText ?? "n/a";
 
-    const company = document.querySelector(".job-details-jobs-unified-top-card__company-name")?.innerText ?? "n/a";
-    const role = document.querySelector(".job-details-jobs-unified-top-card__job-title")?.innerText ?? "n/a";
-    return { salary, company, role, location };
+      const locationParentElement = document.querySelector(".job-details-jobs-unified-top-card__primary-description-container");
+      const location = locationParentElement?.querySelector("span.tvm__text--low-emphasis")?.innerText ?? "n/aaa";
+
+      const company = document.querySelector(".job-details-jobs-unified-top-card__company-name")?.innerText ?? "n/a";
+      const role = document.querySelector(".job-details-jobs-unified-top-card__job-title")?.innerText ?? "n/a";
+      
+      return { salary, company, role, location };
+    }
   }
 
   //go into the browser's current tab to execute javascript
@@ -37,8 +46,8 @@ document.getElementById("scrape-button").addEventListener("click", async () => {
     .executeScript({
       target: { tabId },
       func: modifyDOM,
+      args: [domain],
     })
     //return back to extension
     .then((results) => console.log("returned from browser tab", results[0].result));
-    
 });
